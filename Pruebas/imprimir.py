3c3b65894600df9bf5457ab39c27691cbf44f2f9
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2022-08-12 18:38:26 -0300

    Add files via upload

--- Pruebas/imprimir.py
+++ Pruebas/imprimir.py
@@ -1,3 +1,5 @@
+#Impremir en la consola contenido
+
 print('Hola, soy Juan')
 print('Estoy empezando el curso de python')
 print('Espero aprender mucho') 
---
+++ Pruebas/suma3.py
@@ -0,0 +1,4 @@
+#numero1 = 5 y mostrar su variable
+#numero2 = 6.5 y mostrar su variable
+#numero3 = 7 y mostrar su variable
+#suma de los tres y mostrar el resultado y mostrar su variable
