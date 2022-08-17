4194cac2e96bdb0529edc1ca20117378711f3252
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2022-08-17 16:14:26 -0300

    +++

--- Pruebas/suma3.py
+++ Pruebas/suma3.py
@@ -1,4 +1,13 @@
-#numero1 = 5 y mostrar su variable
-#numero2 = 6.5 y mostrar su variable
-#numero3 = 7 y mostrar su variable
-#suma de los tres y mostrar el resultado y mostrar su variable
+#numero1 = 5 y mostrar su valor
+#numero2 = 6.5 y mostrar su valor
+#numero3 = 7 y mostrar su valor
+#suma de los tres y mostrar el resultado
+numero1=5
+numero2=6.5
+numero3=7
+suma= numero1 + numero2 + numero3
+
+print('primer numero es',numero1)
+print('segundo numero es',numero2)
+print('tercer numero es',numero3)
+print('suma de los tres numeros=',suma)
---
+++ Pruebas/variables.py
@@ -0,0 +1,5 @@
+#Jugando con variables
+
+nombre = "Juan"
+edad = 33
+print('Me llamo', nombre, 'y tengo', edad, 'años.')
