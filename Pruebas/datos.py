d0ed3e5b7844ebd0095b81893cf34c4589099911
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2022-08-18 12:29:06 -0300

    agregado

---
+++ Pruebas/datos.py
@@ -0,0 +1,14 @@
+'''
+Para este ejercicio tenéis que crear en la consola de 
+python variables que representen los siguientes datos de un contacto:
+Nombre
+Apellidos
+Edad
+Email
+Teléfono
+Casado (verdadero o falso)
+Con Hijos (verdadero o falso)
+Lista de amigos
+Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+Una vez hayas creado todas las variables, muéstralas por consola haciendo uso de la función print().
+'''
