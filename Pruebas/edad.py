b339ff350b5001e87be0d02a8bd712ab0b261623
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2022-08-19 15:17:13 -0300

    Add files via upload

---
+++ Pruebas/edad.py
@@ -0,0 +1,11 @@
+'''
+Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+'''
+
+print("Escriba su edad")
+edad = int(input())
+if (edad >=18):
+    print("Es mayor de edad...")
+else:
+    print("No es mayor de edad...") 
+
