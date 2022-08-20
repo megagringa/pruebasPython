8a537614edfa22ee68b46e74d67b3f2ea685f8b2
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2022-08-19 23:19:26 -0300

    +++

--- Pruebas/area.py
+++ Pruebas/area.py
@@ -3,14 +3,14 @@ Escribe una función que calcule el área de un triángulo,
 recibiendo la altura y la base como parámetros y 
 otra función que calcule el área de un círculo recibiendo el radio del mismo.
 '''
-base = input("Base:")
-altura = input("Altura:")
+import math
 
+def area_triangulo(altura: float, base: float) -> float:
+    return ((altura*base)/2)
 
-def area_triangulo(base,altura):
-    return (base*altura)/2
-    
+def area_circulo(radio: float) -> float:
+    return (math.pi*(radio**2))
 
-print(area_triangulo(base,altura))
-
+print(f"Área de triangulo: {area_triangulo(2,2)}")
+print(f"Área del círculo: {area_circulo(4)}")
     
